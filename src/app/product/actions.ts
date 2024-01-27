b77fca4bf9c9f2a4edf36da318012f2f2e051230
Gabriel Lima <gabriel.rslima10@gmail.com>
2024-01-27 17:35:12 -0300

    feat: define logic to add-product

--- src/app/product/actions.ts
+++ src/app/product/actions.ts
@@ -1,12 +1,62 @@
 'use server'
+import { sql } from '@vercel/postgres'
 
-export async function createProduct(formData: FormData) {
-	console.log(formData.get('name'))
+import { ProductEntity } from '@/entities/product.entity'
+import { ProductSchema } from '@/schemas/product.schema'
+
+type FieldErrors = Partial<{
+		[P in keyof Omit<ProductEntity, 'id'>]: string;
+		}>;
+export type FormState = {
+	errors?: {
+		fields?: FieldErrors
+		submit?: string
+	}
+}
+type Fields = Partial<Omit<ProductEntity, 'id'>>
+
+function getFieldsValue(formData: FormData): Fields {
+	const stock = formData.get('stock')?.toString()
+	return {
+		name: formData.get('name')?.toString() || undefined,
+		imageUrl: formData.get('imageUrl')?.toString() || undefined,
+		category: formData.get('category')?.toString() || undefined,
+		price: formData.get('price')?.toString() || undefined,
+		stock: stock ? Number(stock) : undefined,
+	}
+}
+
+function validate(product: Fields) {
+	const validatedProduct = ProductSchema.safeParse(product)
+	if (!validatedProduct.success) {
+		const { fieldErrors } = validatedProduct.error.flatten()
+		const refinedErrors = Object.entries(fieldErrors).reduce((acc, current) => {
+			const [key, errors] = current
+			acc = { ...acc, [key]: errors[0] }
+			return acc
+		}, {} as FieldErrors)
+		return { fieldErrors: refinedErrors  }
+	}
+	return {}
+}
+
+export async function createProduct(prevState: FormState, formData: FormData): Promise<FormState> {
+	const product = getFieldsValue(formData)
+	const { fieldErrors } = validate(product)
+	if (fieldErrors) return { errors: { fields: fieldErrors } }
+	try {
+		await sql`
+			INSERT INTO proucts (name, imageUrl, price, category, stock)
+			VALUES (${product.name}, ${product.imageUrl}, ${product.price}, ${product.category}, ${product.stock})
+		`
+		return {}
+	} catch (err) {
+		return { errors: { submit: 'An unexpected error occurred please try again later' } }
+	}
 }
 
 export async function editProduct(id: string, formData: FormData) {
-	console.log(id)
-	console.log(formData)
+	console.log(id, formData)
 }
 
 export async function deleteProduct(id: string) {
--- src/app/ui/components/product-actions-dialog/add-product.tsx
+++ src/app/ui/components/product-actions-dialog/add-product.tsx
@@ -1,10 +1,14 @@
+'use client'
+import { useFormState } from 'react-dom'
+
+import { Button, TextError } from '@/app/ui/atoms'
 import { ProductDialog } from '../product-dialog'
 import { ProductForm } from '../product-form'
-import { Button } from '@/app/ui/atoms'
 
-import { createProduct } from '@/app/product/actions'
+import { FormState, createProduct } from '@/app/product/actions'
 
 export function AddProduct() {
+	const [state, formAction] = useFormState<FormState>(createProduct, { message: null })
 	return (
 		<ProductDialog.Root>
 			<ProductDialog.Trigger>
@@ -12,14 +16,15 @@ export function AddProduct() {
 			</ProductDialog.Trigger>
 			<ProductDialog.Content className='flex flex-col gap-8'>
 				<ProductDialog.Header title='New Product' description='Fill in the fields to add a new product' />
-				<ProductForm.Root action={createProduct}>
+				<ProductForm.Root action={formAction}>
 					<ProductForm.Fieldset>
-						<ProductForm.Fields.Name />
-						<ProductForm.Fields.ImageUrl />
-						<ProductForm.Fields.Price />
-						<ProductForm.Fields.Category />
-						<ProductForm.Fields.Stock />
+						<ProductForm.Fields.Name errorMessage={state.errors?.fields?.name} />
+						<ProductForm.Fields.ImageUrl errorMessage={state.errors?.fields?.imageUrl} />
+						<ProductForm.Fields.Price errorMessage={state.errors?.fields?.price} />
+						<ProductForm.Fields.Category errorMessage={state.errors?.fields?.category} />
+						<ProductForm.Fields.Stock errorMessage={state.errors?.fields?.stock} />
 					</ProductForm.Fieldset>
+					{state.errors?.submit && <TextError>{state.errors.submit}</TextError>}
 					<ProductDialog.Footer>
 						<ProductDialog.Close>Cancel</ProductDialog.Close>
 						<ProductForm.SubmitButton>Add</ProductForm.SubmitButton>
