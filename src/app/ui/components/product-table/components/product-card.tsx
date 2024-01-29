caaa1c36494b7db9d2cc6d9fa6b6a04640488357
Gabriel Lima <gabriel.rslima10@gmail.com>
2024-01-29 18:43:21 -0300

    fix: add validation to empty image-urls in product-card component

--- src/app/ui/components/product-table/components/product-card.tsx
+++ src/app/ui/components/product-table/components/product-card.tsx
@@ -2,14 +2,18 @@ import Image from 'next/image'
 
 type ProductCardProps = {
 	name: string
-	imageUrl: string
+	imageUrl?: string
 }
 
 export function ProductCard(props: ProductCardProps) {
 	const { name, imageUrl } = props
 	return (
 		<div className='flex items-center gap-4'>
-			<Image src={imageUrl} alt={`Image of ${name}`} width={40} height={40} />
+			{imageUrl ? (
+				<Image src={imageUrl} alt={`Image of ${name}`} width={40} height={40} />
+			) :
+				<div className='block w-[40px] h-[40px] bg-input rounded'></div>
+			}
 			<p>{name}</p>
 		</div>
 	)
