8cf85024ac4781bcece685c04c0352af6f871143
Gabriel Lima <gabriel.rslima10@gmail.com>
2024-02-02 13:19:16 -0300

    fix: add way to handle valid and invalid decimal places in parse-decimal helper function

--- src/helpers/parse-decimal.ts
+++ src/helpers/parse-decimal.ts
@@ -1,6 +1,17 @@
+function verifyDecimalPlaces(value: string) {
+	const hasTwoDecimalPlaces = value.split('.')[1].length === 2
+	return hasTwoDecimalPlaces ? value : value.replace('.', '').concat('.00')
+}
+
+function removeInvalidPunctuation(value: string) {
+	return value.replaceAll('.', '').replace(',', '.')
+}
+
 export function parseDecimal(value: string) {
 	if (!value) throw new Error('Value is required')
-	const parsedValue = parseFloat(value.replaceAll('.', '').replace(',', '.'))
+	const alreadyHasValidPunctuation = !value.includes(',') && value.includes('.')
+	const normalizedValue = alreadyHasValidPunctuation ? verifyDecimalPlaces(value) : removeInvalidPunctuation(value)
+	const parsedValue = parseFloat(normalizedValue)
 	if (isNaN(parsedValue)) throw new Error('Invalid value type')
 	return parsedValue
 }
